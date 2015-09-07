bb984390c877b45870a3f8c968b204b869591eb5
victorgoswami <victorg.1234@gmail.com>
2015-09-06 19:05:38 -0700

    pins

--- Spaces/app/src/main/java/com/example/victorgoswami/spaces/MapsActivity.java
+++ Spaces/app/src/main/java/com/example/victorgoswami/spaces/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.victorgoswami.spaces;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -60,6 +61,17 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(37.5514, -122.2664)).title("Marker"));
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.5514, -122.2664), 14.0f));
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng point) {
+                // TODO Auto-generated method stub
+                //lstLatLngs.add(point);
+                //mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(point));
+            }
+        });
     }
 }
